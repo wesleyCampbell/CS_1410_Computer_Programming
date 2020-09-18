52e7900a041c54756f35a867c3c5d933e2c3d552
Wesley Campbell <wesley.c781@stu.nebo.edu>
2020-09-18 18:50:55 +0000

    Added door_class.py file

---
+++ Term1/door_class.py
@@ -0,0 +1,7 @@
+class Door:
+    def __init__(self, height, color, is_locked):
+        self.height = height
+        self.color = color
+        self.is_locked = is_locked
+        self.is_open = True
+        
