93236ab31cb4228f2f0020c2014758e00724264b
Wesley Campbell <wesley.c781@stu.nebo.edu>
2020-11-04 19:42:17 +0000

    Added ceaser cipher module.

---
+++ Term2/CeaserCipher/main.py
@@ -0,0 +1,22 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
+ceaser_offset = 8
+ceaser_key = alphabet[ceaser_offset:] + alphabet[:ceaser_offset]
+
+# Map msg with key
+encrypt = lambda msg, key: ''.join(map(lambda c: key[alphabet.index(c)] if c in alphabet else c, msg.lower()))
+
+decrypt = lambda msg, key: ''.join(map(lambda c: alphabet[key.index(c)] if c in alphabet else c, msg.lower()))
+
+
+secret_message = "This is a secret message. 12349"
+
+encrypted_message = encrypt(secret_message, ceaser_key)
+
+decrypted_message = decrypt(encrypted_message, ceaser_key)
+
+print(f"""
+Origional Message: {secret_message},
+Encrypted Message: {encrypted_message},
+Decrypted Message: {decrypted_message}
+Equal: {secret_message.lower() == decrypted_message}
+""")
